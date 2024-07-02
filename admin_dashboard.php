7fc0d76865b3ff69b2f80aa17115dda264951512
Chathusha Mendis <chathushamendis@icloud.com>
2024-07-02 21:54:34 +0530

    validated admin dashboard page before showing content

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,11 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Clothing Store - Admin Dashboard</title>
-</head>
-<body>
-    <h2>Admin Dashboard</h2>
-</body>
-</html>
+<?php
+session_start();
+
+if(isset($_SESSION['admin'])){
+    ?>
+    <!-- admin_dashboard page -->
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Online Clothing Store - Admin Dashboard</title>
+    </head>
+    <body>
+        <h2>Admin Dashboard</h2>
+    </body>
+    </html>
+    <?php
+} else {
+    echo "<script>location.href='admin_signin.php'</script>";
+}
+?>
