3cd204f5d801f410ff54ef186c76df24edc6abd1
Chathusha Mendis <chathushamendis@icloud.com>
2024-07-02 11:03:24 +0530

    Added UserType seeder

---
+++ db/seeds/UserTypeSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Seed\AbstractSeed;
+
+class UserTypeSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'type' => 'Admin'
+            ],
+            [
+                'type' => 'User'
+            ]
+        ];
+
+        $user_type = $this->table('user_type');
+        $user_type->insert($data)->saveData();
+    }
+}
