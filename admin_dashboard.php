d7474d3a354e8caa0f75b7c6d97abc10e705c8f8
Chathusha Mendis <chathushamendis@icloud.com>
2024-07-03 14:03:54 +0530

    [done] admin dashboard - user management design

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -5,15 +5,88 @@ if(isset($_SESSION['admin'])){
     ?>
     <!-- admin_dashboard page -->
     <!DOCTYPE html>
-    <html lang="en">
+    <html lang="en" data-bs-theme="dark">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Online Clothing Store - Admin Dashboard</title>
+        <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
     </head>
-    <body>
-        <h2>Admin Dashboard</h2>
+    <body class="admin-body">
+        <!-- nav bar start -->
+        <?php include 'components/admin-nav.php'; ?>
+        <!-- nav bar end -->
+
+        <div class="col-10">
+            <h2 class="text-center">User Management</h2>
+            <div class="mt-3">
+                <table class="table table-hover">
+                <thead>
+                    <tr>
+                    <th scope="col">User ID</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Mobile</th>
+                    <th scope="col">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sahan</td>
+                        <td>Perera</td>
+                        <td>sahan@gmail.com</td>
+                        <td>0778761458</td>
+                        <td>Active</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sahan</td>
+                        <td>Perera</td>
+                        <td>sahan@gmail.com</td>
+                        <td>0778761458</td>
+                        <td>Active</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sahan</td>
+                        <td>Perera</td>
+                        <td>sahan@gmail.com</td>
+                        <td>0778761458</td>
+                        <td>Active</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sahan</td>
+                        <td>Perera</td>
+                        <td>sahan@gmail.com</td>
+                        <td>0778761458</td>
+                        <td>Active</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sahan</td>
+                        <td>Perera</td>
+                        <td>sahan@gmail.com</td>
+                        <td>0778761458</td>
+                        <td>Active</td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+        </div>
+
+        <!-- footer start -->
+         <div class="fixed-bottom col-12">
+            <p class="text-center">&copy; 2024 online clothing store | All rights reserved</p>
+         </div>
+        <!-- footer end -->
     </body>
+
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/script.js"></script>
     </html>
     <?php
 } else {
---
+++ components/admin-nav.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="assets/img/icon.ico" height="30">
+                Online Clothing Store
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">User Management</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Product Management</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Stock Management</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Reports</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
